35e6edceb2615d692e1ee9aa47f09abd3cd32a74
victoria wang <victoria.s.sun@gmail.com>
2023-03-06 15:59:19 -0800

    feat: Add openai package and my-ts-cli binary
    This commit adds the openai package and my-ts-cli binary to the package-lock.json file. It also includes the @types/node package as a devDependency. Additionally, it updates several packages to their latest versions, including axios, asynckit, combined-stream, and delayed-stream.

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+import { Command } from "commander";
+import { exec } from "child_process";
+
 console.log("Hello, CLI!");
+
+const program = new Command();
+program
+  .name("shinobu")
+  .description("Use AI to write your git commits.")
+  .version("1.0.0");
+
+// Commits will already be staged before running pre-commit hook
