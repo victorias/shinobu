85eb5b8ac2f1172dc584e5288768f0c3d134b263
victoria wang <victoria.s.sun@gmail.com>
2023-03-06 16:05:25 -0800

    Add git diff command to capture staged changes in CLI script
    This commit adds a git diff --staged command to capture staged changes in a variable. The command is executed when the pre-commit hook is run. The staged changes are stored in the gitDiff variable, which can be used later in the script. A note has also been added to clarify that commits will already be staged before running the pre-commit hook.

--- bin/cli.ts
+++ bin/cli.ts
@@ -11,4 +11,14 @@ program
   .description("Use AI to write your git commits.")
   .version("1.0.0");
 
-// Commits will already be staged before running pre-commit hook
+// NOTE: Commits will already be staged before running pre-commit hook
+// Run git diff command and capture output in a variable
+exec("git diff --staged", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing git diff: ${error}`);
+    return;
+  }
+  const gitDiff = stdout;
+
+  console.log(gitDiff);
+});
