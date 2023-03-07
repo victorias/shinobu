425a1a7560e4bad660b893b32d2504ffcb891442
victoria wang <victoria.s.sun@gmail.com>
2023-03-06 16:31:04 -0800

    Add .env.local to .gitignore, load OpenAI API key from .env.local, and install dotenv and path packages.
    Add .env.local to .gitignore to prevent it from being committed.
    Load environment variables from .env.local file using dotenv package and path package to resolve the file path.
    Check if the OpenAI API key is defined in the environment and log an error if it is not defined.
    Install axios package to make HTTP requests and openai package to access OpenAI API.
    Upgrade @types/axios and @types/node packages to the latest versions.

--- bin/cli.ts
+++ bin/cli.ts
@@ -2,6 +2,8 @@
 
 import { Command } from "commander";
 import { exec } from "child_process";
+import dotenv from "dotenv";
+import path from "path";
 
 console.log("Hello, CLI!");
 
@@ -10,6 +12,18 @@ program
   .name("shinobu")
   .description("Use AI to write your git commits.")
   .version("1.0.0");
+
+// Load environment variables from .env.local file
+dotenv.config({ path: path.resolve(__dirname, "../.env.local") });
+
+// Get the open_ai_key variable from the environment
+const openAiKey = process.env.OPENAI_SECRET_KEY;
+
+if (!openAiKey) {
+  console.error(
+    `Error: No Open AI Secret Key found. Have you setup .env.local OPENAI_SECRET_KEY?`
+  );
+}
 
 // NOTE: Commits will already be staged before running pre-commit hook
 // Run git diff command and capture output in a variable
@@ -23,6 +37,7 @@ exec(`git diff --staged ${excludes.join(` `)}`, (error, stdout, stderr) => {
     console.error(`Error executing git diff: ${error}`);
     return;
   }
+
   const gitDiff = stdout;
 
   console.log(gitDiff);
